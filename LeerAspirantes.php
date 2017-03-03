d2419ee01840296d827426f45e24b2b9a5172844
Kike <kike@areavisual.net>
2017-03-03 18:16:01 +0100

    Adaptando a Bootstrap
    Maquetando y mostrando datos.

--- LeerAspirantes.php
+++ LeerAspirantes.php
@@ -2,7 +2,7 @@
 
 session_start();
 if(!isset($_SESSION["user_id"]) || $_SESSION["user_id"]==null){
-	print "<script>alert(\"Acceso invalido!\");window.location='login.php';</script>";
+	print "<script>alert(\"Acceso inválido!\");window.location='login.php';</script>";
 }
 
 include "php/navbar.php";
@@ -25,10 +25,12 @@ include "php/navbar.php";
    <div class="container">   
     <div class="row">
 
+
         <div class="col-md-12">
-            <h2>Listado de todos los Curruculum</h2>
+            <h1>Listado de todos aspirantes</h1>
         </div>
 
+        <hr>
         <br><br>
 
         <form action="" method="post">
@@ -38,42 +40,38 @@ include "php/navbar.php";
             </div>
 
             <br>
+            <div class="col-md-12"> 
 
-            <select class="col-md-6 entradaTxt" name="Pais" data-placeholder="Seleccione país" tabindex="-1" aria-hidden="true" >
-                <option value="" class="selected">Seleccione el País</option>
-                <option value="España">España</option>
-                <option value="Otro">Otro</option>
-            </select>
+                <select class="col-md-6 entradaTxt" name="Pais" data-placeholder="Seleccione país" tabindex="-1" aria-hidden="true" >
+                    <option value="" class="selected">Seleccione el País</option>
+                    <option value="España">España</option>
+                    <option value="Otro">Otro</option>
+                </select>              
 
+                <select class="col-md-6" id="ms" name="CategoriasInteres" data-placeholder="Seleccione categoría de interés" tabindex="-1" aria-hidden="true" >
+                    <option value="" >Selecciona Categoría de Interés</option>
+                        <option value="Adminstración"       >Adminstración</option>
+                        <option value="Comercial"           >Comercial</option>
+                        <option value="Calidad"             >Calidad</option>
+                        <option value="I+D+I"               >I+D+I</option>
+                        <option value="Recursos Humanos"    >Recursos Humanos</option>
+                        <option value="Área financiera"     >Área financiera</option>
+                </select>
 
-            <br><br>                
-
-            <select class="col-md-6" id="ms" name="CategoriasInteres" data-placeholder="Seleccione categoría de interés" tabindex="-1" aria-hidden="true" >
-                <option value="" >Selecciona Categoría de Interés</option>
-                    <option value="Adminstración"       >Adminstración</option>
-                    <option value="Comercial"           >Comercial</option>
-                    <option value="Calidad"             >Calidad</option>
-                    <option value="I+D+I"               >I+D+I</option>
-                    <option value="Recursos Humanos"    >Recursos Humanos</option>
-                    <option value="Área financiera"     >Área financiera</option>
-            </select>
+            </div>
 
             <br><br>
 
             <div class="col-md-12">
                     <input class="btn btn-default" type="submit" name="Buscar" value ="Buscar Registros">
+                   
             </div>            
 
         </form> 
 
-        <br>
+        
 
-        <div class="col-md-12">
-            <!-- Volver apantalla inicial -->
-            <br><br>
-            <input class="btn btn-default" type="button" value="Volver al menú principal" onclick="location.href='GestorPrincipal.php'">
-            <br><br>
-        </div>  
+        <br><br>
             
 
             
@@ -84,7 +82,7 @@ include "php/navbar.php";
             <div class="col-md-2"> <label> &nbsp;<B>País</B>&nbsp;</label><br/> </div>
             <div class="col-md-2"> <label> &nbsp;<B>Provincia</B>&nbsp;</label><br/> </div>
             <div class="col-md-2"> <label> &nbsp;<B>Municipio</B>&nbsp;</label><br/> </div>
-            <div class="col-md-2"> <label> &nbsp;<B>ID usuario</B>&nbsp;</label><br/> </div>
+            <div class="col-md-2"> <label> &nbsp;<B>ID / Perfil</B>&nbsp;</label><br/> </div>
 
         </div>
 
@@ -115,16 +113,22 @@ include "php/navbar.php";
             $resultado_consulta_mysql=mysqli_query($link, $consulta_mysql);
             
              while (($fila = mysqli_fetch_array($resultado_consulta_mysql))!=NULL){
+       ?>
+                    
+        <div class="row show-Migrid">
+            
 
+            <div class="col-md-2"> <?php echo $fila['Nombre'];?> <br/> </div>
+            <div class="col-md-2"> <?php echo $fila['Apellidos'];?> <br/> </div>
+            <div class="col-md-2"> <?php echo $fila['Pais'];?> <br/> </div>
+            <div class="col-md-2"> <?php echo $fila['Provincia'];?> <br/> </div>
+            <div class="col-md-2"> <?php echo $fila['Municipio'];?> <br/> </div>
+            <div class="col-md-2"> <?php echo $fila['id'];?><a href="./VerRegistro.php?id=<?php echo $fila['id'];?>" > Ver perfil </a> <br/> </div>
 
-                       printf("<tr> 
-                       <td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td><a href=\"VerRegistro.php?id=%d\"> Ver registro </a></td>
-                            </tr>",          
-                        $fila['Nombre'], $fila['Apellidos'], $fila['Pais'], $fila['Provincia'], $fila['Municipio'],
-                        $fila['CategoriasInteres'],
-                        $fila['id']);
+        </div>
 
-                    }
+        <?php 
+            }
 
                     # Libero la memoria asociada a result y cierro base de datos
                     mysqli_free_result($resultado_consulta_mysql);
@@ -150,7 +154,7 @@ include "php/navbar.php";
             <div class="col-md-2"> <?php echo $fila['Pais'];?> <br/> </div>
             <div class="col-md-2"> <?php echo $fila['Provincia'];?> <br/> </div>
             <div class="col-md-2"> <?php echo $fila['Municipio'];?> <br/> </div>
-            <div class="col-md-2"> <?php echo $fila['id'];?><a href="./VerRegistro.php?id=<?php echo $fila['id'];?>" > Ver registro </a> <br/> </div>
+            <div class="col-md-2"> <?php echo $fila['id'];?><a href="./VerRegistro.php?id=<?php echo $fila['id'];?>" > Ver perfil </a> <br/> </div>
 
         </div>
 
@@ -169,8 +173,15 @@ include "php/navbar.php";
 
         ?>
 
-
-            
+        <br><br>
+        <hr>
+        <br><br>
+        <div align="center">
+            <!-- Volver apantalla inicial -->
+            <input class="btn btn-default" type="button" value="Volver al menú principal" onclick="location.href='GestorPrincipal.php'">
+            <br><br>
+        </div>
+        
  
             
     </body>
